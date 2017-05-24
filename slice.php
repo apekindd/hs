6466b951de584610a0c110e7da74752ec018b0eb
apekin-pc\apekin <apekin@uamaster.com>
2017-05-24 10:05:09 +0300

    crep png

--- slice.php
+++ slice.php
@@ -3,10 +3,13 @@ include('functions/crop.php');
 include('functions/resize_png.php');
 
 //$file=$_SERVER['DOCUMENT_ROOT'].'/data/ru/AT_001t.png';
-$file=$_SERVER['DOCUMENT_ROOT'].'/data/ru/AT_001t.png';
+$file=$_SERVER['DOCUMENT_ROOT'].'/data/ru/LOE_027.png';
 
 
 
 resizePng(200,303,$file, $file);
 
-cropPng($file,40,80,120,40,$_SERVER['DOCUMENT_ROOT'].'/data/ru/AT_001tt.png', 200, 40);
+//spell
+cropPng($file,40,80,120,40,$_SERVER['DOCUMENT_ROOT'].'/data/ru/AT_001nn'.time().'.png', 200, 40);
+//minion
+//cropPng($file,40,68,120,40,$_SERVER['DOCUMENT_ROOT'].'/data/ru/AT_001'.time().'.png', 200, 40);
